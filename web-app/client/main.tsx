b39874bd292bbbd8e0d08f2d73461ade0339cc3d
finista <95535357+finista@users.noreply.github.com>
2024-12-23 10:24:06 +0300

    Fix imports

--- web-app/client/main.tsx
+++ web-app/client/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 
-import App from "@imports/components/ui/app"
+import App from "@imports/core/app"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
