64e30e0ff4449f7fd3a0dd30bbaa33ff248768ba
finista <95535357+finista@users.noreply.github.com>
2024-12-24 21:40:01 +0300

    Added products frame and products context

--- web-app/imports/pages/Home.tsx
+++ web-app/imports/pages/Home.tsx
@@ -1,10 +1,15 @@
 import Navbar from "@imports/components/shared/navbar"
+import ProductsFrame from "@imports/components/ui/products_frame"
+import ProductsProvider from "@imports/components/shared/products_context"
 
 const Home = () => {
   return (
     <>
       <Navbar />
-      <div>Home</div>
+      <h3>Home! :3</h3>
+      <ProductsProvider>
+        <ProductsFrame />
+      </ProductsProvider>
     </>
   )
 }
