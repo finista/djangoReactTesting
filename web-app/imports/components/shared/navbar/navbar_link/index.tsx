aa21cf9445a28f8dec034f28e18b63ed7641ff1d
Finista <sonne.ru2000@gmail.com>
2025-01-06 01:53:15 +0300

    Changed links to use Link from react dom instead of HTML <a />

--- web-app/imports/components/shared/navbar/navbar_link/index.tsx
+++ web-app/imports/components/shared/navbar/navbar_link/index.tsx
@@ -1,9 +1,10 @@
+import { Link } from "react-router-dom"
 import { LinkProps } from "./types"
 
 const NavbarLink = (props: LinkProps) => {
   return (
     <li className="nav-item">
-        <a href={props.linkTarget} className="nav-link">{props.linkText}</a>
+        <Link to={props.linkTarget} className="nav-link">{props.linkText}</Link>
     </li>
   )
 }
