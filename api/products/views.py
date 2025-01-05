34e82f1d2f96b5eb641fa0089a0004de7405a33b
Finista <sonne.ru2000@gmail.com>
2025-01-05 16:24:22 +0300

    Added delay simulation

--- api/products/views.py
+++ api/products/views.py
@@ -4,9 +4,13 @@ from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
 
+from time import sleep
+
 class GetProducts(APIView):
     def get(self, request, *args, **kwargs):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
+        
+        sleep(2)
         
         return Response(serializer.data, status=status.HTTP_200_OK)
